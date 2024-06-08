5994ab329a7c78a3b4ce6bbd363e6fcbb926d2df
citrogabriel <citrogabriel@gmail.com>
2024-06-07 22:05:26 -0300

    cambios

--- Js/micarrito.js
+++ Js/micarrito.js
@@ -6,6 +6,19 @@ console.log(cartStorage)
 
 let cartContainer = document.getElementById("cart-section")
 
+function renderCarrito (carditems) {
+    carditems.forEach (producto => {
+        const cart = document.createElement("div")
+        cart.innerHTML = `<h3>${products.descripcion}</h3>
+                          <h4>${products.precio}</h4>`
+                          cartContainer.appendChild(cart)
+
+
+    })
+}
+renderCarrito(cartStorage)
+
+
 
 
 
