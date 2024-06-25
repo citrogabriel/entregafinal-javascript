ac1772a9208f2512a87c2c25169294516e2145b8
citrogabriel <citrogabriel@gmail.com>
2024-06-25 17:40:54 -0300

    entraga corregida

--- Js/micarrito.js
+++ Js/micarrito.js
@@ -1,22 +1,27 @@
-let cartStorage = localStorage.getItem("cartProducts")
+let cartStorage = localStorage.getItem("cartProducts");
 
+cartStorage = JSON.parse(cartStorage);
 
-cartStorage = JSON.parse(cartStorage)
-console.log(cartStorage)
+let cartContainer = document.getElementById("cart-section");
 
-let cartContainer = document.getElementById("cart-section")
-
-function renderCarrito (carditems) {
-    carditems.forEach (producto => {
-        const cart = document.createElement("div")
-        cart.innerHTML = `<h3>${products.descripcion}</h3>
-                          <h4>${products.precio}</h4>`
-                          cartContainer.appendChild(cart)
-
-
-    })
+function renderCarrito(carditems) {
+  carditems.forEach((producto) => {
+    const cart = document.createElement("div");
+    cart.innerHTML = `<h3>${producto.descripcion}</h3>
+                          <h4>${producto.precio}</h4>
+                          <button type="button" class="btn btn-success" onClick="eliminarProducto(this.id)" id="${producto.id}">Eliminar</button>`;
+    cartContainer.appendChild(cart);
+  });
 }
-renderCarrito(cartStorage)
+renderCarrito(cartStorage);
+
+function eliminarProducto(productId) {
+  const resultado = cartStorage.filter((producto) => producto.id != productId);
+  cartStorage = localStorage.setItem("cartProducts", JSON.stringify(resultado));
+
+  window.location.reload();
+}
+
 
 
 
